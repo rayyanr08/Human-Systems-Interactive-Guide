4bcbe8dbea433b74e510c8014bf28d5b8dc8f014
rayyanr08 <rayyanrahman26@gmail.com>
2025-05-28 10:55:12 -0400

    fixed a HUGE problem regarding the handling of input events from user and organized collision objects to area nodes

--- scripts/Right Femur.gd
+++ scripts/Right Femur.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 
-func _on_character_body_3d_input_event(camera, event, position, normal, shape_idx):
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("YOU CLICKED THE RIGHT FEMUR")

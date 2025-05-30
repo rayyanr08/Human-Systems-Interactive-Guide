01c6706943c40e98bef42fa847f1031208d93e26
rayyanr08 <rayyanrahman26@gmail.com>
2025-05-30 10:52:59 -0400

    working on transitioning cameras so that user can look at the 3dview of the skeleton

---
+++ scripts/Camera3D.gd
@@ -0,0 +1,12 @@
+extends Camera3D
+const MOVE_SPEED = 200
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
